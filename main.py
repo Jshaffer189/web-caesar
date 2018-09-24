f2064e5cf5c00bcb4e29d2634d7fb570dfa8b960
joshuashaffer189 <jshaffer189@yahoo.com>
2018-09-24 14:06:08 -0500

    finished web caesar

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -34,7 +34,7 @@ form = """
     </body>
 </html>
 """
-@app.route("/", methods=['POST'])
+@app.route("/encrypt", methods=['POST'])
 def encrypt():
     rot = request.form['rot']
     rot = int(rot)
@@ -45,7 +45,5 @@ def encrypt():
 @app.route("/")
 def index():
     return form.format("")
-
-
 
 app.run()
